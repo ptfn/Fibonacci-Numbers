3f37a56742af2b75592c201588481e5c9dc706f4
D34D_FR13ND5 <CipherDogs@protonmail.com>
2020-12-12 20:13:27 +0700

    Add files via upload

---
+++ fib2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int fib (int n){
+    double num;
+    return num = (pow(((1+sqrt(5)) / (2)),n) - pow(((1-sqrt(5)) / (2)),n)) / sqrt(5);
+}
+
+int main(){
+    printf("Number Fib:%d\n",fib(40));
+    return 0;
+}
